62c3f2d53827d16fa1cb14fc84ff9c15e44db03b
cwarrior010 <farisichsanalyawa@gmail.com>
2024-11-25 14:18:55 +0700

    Update MyHanoi.c

--- MyHanoi.c
+++ MyHanoi.c
@@ -1,14 +1,1 @@
-#include "Hanoi.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) 
-{
-	printf("Alhamdulillah tidak ada eror\n");
-	SizeRect();
-	SetChar();
-	SetSulit();
-	
-	
-	return 0;
-}
