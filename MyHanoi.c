367057b58dc97dd6571164098adde570e4638e3b
Nabilpnyk <mns.rasyiq03@gmail.com>
2024-11-25 14:15:22 +0700

    Add files via upload

---
+++ MyHanoi.c
@@ -0,0 +1,14 @@
+#include "Hanoi.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	printf("Alhamdulillah tidak ada eror\n");
+	SizeRect();
+	SetChar();
+	SetSulit();
+	
+	
+	return 0;
+}
